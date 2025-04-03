542f8f15d1031323e746c7972641c62d1e0cb9c6
Ishita Chauhan <ishitachauhan1407@gmail.com>
2025-04-03 17:50:07 +0530

    variables

---
+++ Chapter_01/index_variables.js
@@ -0,0 +1,13 @@
+console.log("This contains code for the Ultimate Javascript course video no 2")
+
+let a = 67 // a is a variable which contains 67
+console.log(a)
+a = "harry"
+console.log(a)
+
+//let is used for declaring variable
+
+//variable name can start with- letter, _ , $
+//variable name can contain- letter, nos., _ , $
+//variable name cannot be reserved words
+//Case-sensitive
